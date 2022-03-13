b767c26bab61be0b2d0184681d6a45d3bbd511a6
Mahesh Sampekatte <mseenapp@mathworks.com>
2022-03-13 01:53:26 -0500

    js examples

--- examples/flatten-nested-array-example.js
+++ examples/flatten-nested-array-example.js
@@ -5,6 +5,9 @@
  * let arr = [1,2,[3,4,[5,6],7],8,[9,[10,11]]]
  * Output: [1,2,3,4,5,6,7,8,9,10,11]
  *
+ * Reference:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+ *
  */
 
 function flatten(arr, resultArray = []) {
