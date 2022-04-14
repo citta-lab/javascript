9d6a1eada94392324e76cd555d5aca654e490ffa
citta-lab <mahesh2684@gmail.com>
2022-04-14 08:19:47 -0400

    Update hoisting-example.js

--- examples/hoisting-example.js
+++ examples/hoisting-example.js
@@ -12,16 +12,24 @@ function exampleHoist(){
   /** variable declaration and assignment */
   var a = 20;
   
-  /** function expression */
+  /** function declaration */
   function b(){
       return 21
   }
 
-  /** function declaration and assignment */
+  /** function expression */
   var c = function(){
       return 23
   }
 }
 
+/** 
+  Reason:
+  - Variable names ( with var ) get hoisted to top of the file before runtime.
+  i.e only the left part of it and hence we get 'undefined' rather than reference error
+  - Function declarations ( without assignment ) will get hoisted tot the top.
+  - Function expressions acts similar to variable and only the left part of it will get
+  hoisted
+  */
 
 
