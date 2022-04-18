dfcc3767b5dcd1115f79fb848c6825a986ded2c2
citta-lab <mahesh2684@gmail.com>
2022-04-18 07:54:46 -0400

    Update bind-complete-example.js

--- examples/bind-complete-example.js
+++ examples/bind-complete-example.js
@@ -60,3 +60,31 @@ setTimeout(customGreet, 0); // Bob welcomes you
 setTimeout(() => {
   customGreet();
 }, 0); // Bob welcomes you
+
+
+/**
+ * ******************************************************************
+ * Bind in Prototype.js
+ * ******************************************************************
+ */
+Function.prototype.bind = function(){ 
+  var fn = this, args = Array.prototype.slice.call(arguments), object = args.shift(); 
+  return function(){ 
+    return fn.apply(object, 
+      args.concat(Array.prototype.slice.call(arguments))); 
+  }; 
+}; 
+
+// calling above bind in an example
+var Button = { 
+  click: function(value){ 
+    this.clicked = value; 
+  } 
+}; 
+ 
+var elem = document.createElement("li"); 
+elem.innerHTML = "Click me!"; 
+elem.onclick = Button.click.bind(Button, false); 
+document.getElementById("results").appendChild(elem); 
+ 
+elem.onclick(); 
